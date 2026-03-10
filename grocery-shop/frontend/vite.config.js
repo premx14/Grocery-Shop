9c4171d928a1934586eac0575060484fe6c30899
premx14 <premgaikar14@gmail.com>
2026-03-10 23:49:35 +0530

    Update vite.config.js

--- grocery-shop/frontend/vite.config.js
+++ grocery-shop/frontend/vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: { '/api': { target: 'http://localhost:5000', changeOrigin: true } },
+    proxy: { '/api': { target: 'https://grocery-shop-x7sf.onrender.com', changeOrigin: true } },
   },
 });
